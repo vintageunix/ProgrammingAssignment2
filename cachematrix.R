eebc52a43ab895e2d92617f5be732e263343c839
vintageunix <krishnan@Sankarans-iMac.local>
2014-12-11 17:11:09 +0900

    comment added

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## Adding a comment to commit check on git
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
